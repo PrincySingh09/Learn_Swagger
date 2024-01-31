71937bee83f0949b467e18b8e0f22c76b20ace30
PrincySingh09 <139205001+PrincySingh09@users.noreply.github.com>
2024-01-31 12:03:41 +0530

    UserNotFoundException.java

--- src/main/java/com/rest/swagger_assignment/exception/UserNotFoundException.java
+++ src/main/java/com/rest/swagger_assignment/exception/UserNotFoundException.java
@@ -1,6 +1,5 @@
 package com.rest.swagger_assignment.exception;
 
-//UserNotFoundException.java (Custom Exception)
 public class UserNotFoundException extends RuntimeException {
  public UserNotFoundException(String message) {
      super(message);
